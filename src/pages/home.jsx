50921b780838fda032c1a10a634a335310a3c40f
Rfontt <ritafontenele.infor2@gmail.com>
2023-05-28 15:57:19 -0300

    feat: add components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,36 +1,68 @@
 // Felipe
 import { useEffect, useState } from 'react'
-import { Container, Title, Header } from '../styles/home'
+import { Container, Title, Header, Content, List } from '../styles/home'
 import axios from 'axios'
+import Logo from '../images/logo.png'
 
 export default function Home() {
     const [products, setProducts] = useState([
         {
-            name: 'Maça',
-            type: 'Fruta'
+            name: 'Maças',
+            type: 'Fruta',
+            img: 'public/'
         },
 
         {
-            name: 'Limão',
-            type: 'Fruta'
+            name: 'Limões',
+            type: 'Fruta',
+            img: 'public/'
+        },
+
+        {
+            name: 'Uvas',
+            type: 'Fruta',
+            img: 'public/'
+        },
+
+        {
+            name: 'Peixes',
+            type: 'Peixe',
+            img: 'public/'
+        },
+
+        {
+            name: 'Carnes',
+            type: 'Carne',
+            img: 'public/'
+        },
+
+        {
+            name: 'Leites',
+            type: 'Leite',
+            img: 'public/'
         }
     ])
 
     return (
         <Container>
             <Header>
-                <h2>GrowUp</h2>
+                <img src={Logo} />
             </Header>
 
-            {
-               products.length == 0 ? <h1>Loading...</h1> :  
-               products.map((item) => {
-                return <div>
-                    <h1>{item.name}</h1>
-                    <span>{item.type}</span>
-                </div>
-                })
-            }
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <Content>
+                    {
+                        products.length == 0 ? <h1>Loading...</h1> :  
+                        products.map((item) => {
+                            return <List>
+                                <Title>{item.name}</Title>
+                                <span>{item.type}</span>
+                                <button>Visualizar</button>
+                            </List>
+                        })
+                    }
+                </Content>
+            </div>
         </Container>
     )
 }
